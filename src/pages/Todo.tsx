1153defb50af43111535d959ea40e27beffc5a66
ZahirulIslamJoy <neloy1613@gmail.com>
2024-08-23 20:16:15 +0600

    added delete todo

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,37 +1,73 @@
-import { addTodo } from "@/redux/features/todoSlice";
-import { useAppDispatch } from "@/redux/hooks";
+import { addTodo, deleteTodo } from "@/redux/features/todoSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { FormEvent } from "react";
 
 const Todo = () => {
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
+  const { todos } = useAppSelector((state) => state.todos);
+  console.log(todos);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const title = (
+      e.currentTarget.elements.namedItem("title") as HTMLInputElement
+    ).value;
+    const description = (
+      e.currentTarget.elements.namedItem("description") as HTMLInputElement
+    ).value;
+    const id = Math.random().toString(36).substring(2, 7);
+    const data = {
+      id,
+      title,
+      description,
+    };
+    dispatch(addTodo(data));
+  };
 
 
-    const handleSubmit=(e: FormEvent<HTMLFormElement>)=>{
-        e.preventDefault()
-        const title = (e.currentTarget.elements.namedItem('title') as HTMLInputElement).value;
-        const description = (e.currentTarget.elements.namedItem('description') as HTMLInputElement).value;
-        const data = {
-            title , description
-        }
-        dispatch(addTodo(data))
-       
-    }
+  const handleDelete=(id : string)=>{
+    dispatch(deleteTodo(id));
+  }
 
-    return (
-        <div className="p-10">
-            <form onSubmit={handleSubmit}>
-                <div className="flex items-center gap-4">
-                <label  htmlFor="">Title</label>
-                <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5  " type="text" name="title" />
-                </div>
-                <div className="flex items-center gap-4 mt-8">
-                <label htmlFor="">Description</label>
-                <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5  " type="text" name="description" />
-                </div>
-                <button className="px-4 text-white bg-blue-300 rounded-xl py-2" type="submit">Submit</button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <div className="p-10">
+        <form onSubmit={handleSubmit}>
+          <div className="flex items-center gap-4">
+            <label htmlFor="">Title</label>
+            <input
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5  "
+              type="text"
+              name="title"
+            />
+          </div>
+          <div className="flex items-center gap-4 mt-8">
+            <label htmlFor="">Description</label>
+            <input
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5  "
+              type="text"
+              name="description"
+            />
+          </div>
+          <button
+            className="px-4 text-white bg-blue-300 rounded-xl py-2"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id} className="flex gap-4 bg-blue-300 p-4 m-4">
+            <p>{todo.title}</p>
+            <p>{todo.description}</p>
+            <button onClick={()=>handleDelete(todo.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
--- src/redux/features/todoSlice.ts
+++ src/redux/features/todoSlice.ts
@@ -1,6 +1,7 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit"
 
 type TTodos = {
+    id:string
     title:string,
     description:string,
     isCompleted?:boolean
@@ -21,9 +22,12 @@ const todoSlice=createSlice({
     reducers:{
         addTodo:(state , action : PayloadAction<TTodos>)=>{
             state.todos.push({...action.payload , isCompleted:false})
+        },
+        deleteTodo:(state, action:PayloadAction<string>)=>{
+            state.todos= state.todos.filter((item)=>item.id != action.payload)
         }
     }
 })
-export const {addTodo}=todoSlice.actions
+export const {addTodo , deleteTodo}=todoSlice.actions
 
 export default todoSlice.reducer;
